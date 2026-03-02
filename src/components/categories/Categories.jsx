cf10e16eaf0af0f611c776abd88a1ac4c7ae945b
ManalTalli <manal5talli@gmail.com>
2026-03-02 13:36:20 +0200

    edit cat

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -7,6 +7,6 @@ export default function Categories() {
     if (isLoading) return <CircularProgress/>
     if (isError) return <Box color={'red'}>{error.message}</Box>
   return (
-    <Box>{data.response.map (category=> <Box key={category.id}>{category.name} </Box>)}</Box>
+    <Box>{data.response.data.map (category=> <Box key={category.id}>{category.name} </Box>)}</Box>
   )
 }
